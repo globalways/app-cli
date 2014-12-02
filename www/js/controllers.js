06eb1060048c5fec3a2f005f0ff9c9855a05ace9
JamesFan <971797140@qq.com>
2014-12-02 22:50:22 +0800

    添加GasStaions列表

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,12 +3,12 @@ angular.module('starter.controllers', [])
 .controller('DashCtrl', function($scope) {
 })
 
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
+.controller('StationsCtrl', function($scope, Stations) {
+  $scope.stations = Stations.all();
 })
 
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
+.controller('StationDetailCtrl', function($scope, $stateParams, Stations) {
+  $scope.station = Stations.get($stateParams.stationId);
 })
 
 .controller('AccountCtrl', function($scope) {
